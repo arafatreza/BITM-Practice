fba4acaa3723756de01eac70cbe14f0adb1a1664
arafatreza <arafat.reza1997@gmail.com>
2020-03-10 10:19:30 +0600

    2 project added update

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -20,24 +20,56 @@ namespace Calculator
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            if (firstTextBox.Text == "")
+            {
+                MessageBox.Show("First Number can't be empty");
+            }
+            if (secondTextBox.Text == "")
+            {
+                MessageBox.Show("Second Name can't be empty");
+            }
             var s = Convert.ToInt32(firstTextBox.Text) + Convert.ToInt32(secondTextBox.Text);
             resultTextBox.Text = Convert.ToString(s);/*s.ToString()*/
         }
 
         private void subtractionButton_Click(object sender, EventArgs e)
         {
+            if (firstTextBox.Text == "")
+            {
+                MessageBox.Show("First Number can't be empty");
+            }
+            if (secondTextBox.Text == "")
+            {
+                MessageBox.Show("Second Name can't be empty");
+            }
             var s = Convert.ToInt32(firstTextBox.Text) - Convert.ToInt32(secondTextBox.Text);
             resultTextBox.Text = Convert.ToString(s);/*s.ToString()*/
         }
 
         private void multipicationButton_Click(object sender, EventArgs e)
         {
+            if (firstTextBox.Text == "")
+            {
+                MessageBox.Show("First Number can't be empty");
+            }
+            if (secondTextBox.Text == "")
+            {
+                MessageBox.Show("Second Name can't be empty");
+            }
             var s = Convert.ToInt32(firstTextBox.Text) * Convert.ToInt32(secondTextBox.Text);
             resultTextBox.Text = Convert.ToString(s);/*s.ToString()*/
         }
 
         private void divisionButton_Click(object sender, EventArgs e)
         {
+            if (firstTextBox.Text == "")
+            {
+                MessageBox.Show("First Number can't be empty");
+            }
+            if (secondTextBox.Text == "")
+            {
+                MessageBox.Show("Second Name can't be empty");
+            }
             var s = Convert.ToInt32(firstTextBox.Text) / Convert.ToInt32(secondTextBox.Text);
             resultTextBox.Text = Convert.ToString(s);/*s.ToString()*/
         }

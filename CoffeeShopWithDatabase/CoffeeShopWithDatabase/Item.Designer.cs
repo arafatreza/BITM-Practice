f4f564fcc162b0b1adad2f7b0de084a7600580d3
arafatreza <arafat.reza1997@gmail.com>
2020-03-11 16:39:16 +0600

    CoffeeShopWithDatabase Added successfully

---
+++ CoffeeShopWithDatabase/CoffeeShopWithDatabase/Item.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CoffeeShopWithDatabase
+{
+    partial class Item
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // itemLabel
+            // 
+            this.itemLabel.AutoSize = true;
+            this.itemLabel.Font = new System.Drawing.Font("Times New Roman", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemLabel.Location = new System.Drawing.Point(338, 41);
+            this.itemLabel.Name = "itemLabel";
+            this.itemLabel.Size = new System.Drawing.Size(74, 36);
+            this.itemLabel.TabIndex = 0;
+            this.itemLabel.Text = "Item";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(150, 129);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(72, 25);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceLabel.Location = new System.Drawing.Point(150, 197);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(66, 25);
+            this.priceLabel.TabIndex = 2;
+            this.priceLabel.Text = "Price";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(265, 129);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(215, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(265, 198);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(215, 20);
+            this.priceTextBox.TabIndex = 4;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(405, 256);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // Item
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.itemLabel);
+            this.Name = "Item";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label itemLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button addButton;
+    }
+}
+
---
+++ CoffeeShopWithDatabase/CoffeeShopWithDatabase/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShopWithDatabase
+{
+    public partial class Item : Form
+    {
+        public Item()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string connection = @"Server=DESKTOP-28DHLU6; Database=CoffeeShop; integrated security=true";
+            SqlConnection sqlconnection = new SqlConnection(connection);
+
+            string command = "insert into Items(Name,Price) values ('" + nameTextBox.Text + "','" + priceTextBox.Text + "')";
+            SqlCommand con = new SqlCommand(command, sqlconnection);
+
+            sqlconnection.Open();
+
+            int i = con.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Added");
+            }
+            else
+            {
+                MessageBox.Show("Not Added");
+            }
+
+            sqlconnection.Close();
+
+            clear();
+        }
+        private void clear()
+        {
+            nameTextBox.Clear();
+            priceTextBox.Clear();
+        }
+    }
+}
